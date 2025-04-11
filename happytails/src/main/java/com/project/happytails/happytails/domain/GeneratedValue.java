72d55a880d0853217de69af3a8eae0b8133ce612
SpartanDen <145934559+SpartanDen@users.noreply.github.com>
2025-04-11 06:48:21 +0530

    Delete happytails directory

--- happytails/src/main/java/com/project/happytails/happytails/domain/GeneratedValue.java
+++
@@ -1,7 +0,0 @@
-package com.project.happytails.happytails.domain;
-
-public @interface GeneratedValue {
-
-    String strategy();
-
-}
--- happytails/src/main/java/com/project/happytails/happytails/repository/UserRepository.java
+++
@@ -1,5 +0,0 @@
-package com.project.happytails.happytails.repository;
-
-public class UserRepository {
-    
-}
